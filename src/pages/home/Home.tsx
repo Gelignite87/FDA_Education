5a8ae9b9a1118fea559bbb46470c59ebfa9d4615
dmitrii fomin <railgun87@rambler.ru>
2024-11-28 20:33:25 +1200

    41

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,36 @@
+import { Breadcrumbs } from '../../components/breadcrumbs/Breadcrumbs.tsx'
+import { LeftMenu } from '../../components/left-menu/LeftMenu.tsx'
+import { ToastTypes } from '../../components/toast/ToastTypes.ts'
 import { Banner } from '../../components/banner/Banner.tsx'
+import { Footer } from '../../components/footer/Footer.tsx'
+import { Toast } from '../../components/toast/Toast.tsx'
+import styles from './Home.module.sass'
+import { useState } from 'react'
 
 export const Home: React.FC = () => {
+  //Код для компонента Toast.tsx
+  const [showToast, setShowToast] = useState<ToastTypes>({
+    isShow: false,
+    text: '',
+    color: '',
+  })
+  const [idToast, setIdToast] = useState<number>(0)
+
   return (
     <>
-      <Banner text="Приветствую учащихся!" />
-      <div>Home</div>
+      <div>
+        <Banner text={'Приветствую учащихся!'} />
+        <div className={styles.home_flex}>
+          <div className={styles.home_flexLeft}>
+            <LeftMenu />
+          </div>
+          <div className={styles.home_flexRight}>
+            <Breadcrumbs id={'home_page'} />
+          </div>
+        </div>
+      </div>
+      <Toast props={{ showToast, setShowToast, idToast, setIdToast }} />
+      <Footer />
     </>
   )
 }

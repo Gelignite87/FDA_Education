1b2c923fb6a371002dadf89194f1ce0faba0744b
dmitrii fomin <railgun87@rambler.ru>
2024-11-29 18:14:05 +1200

    46

--- src/pages/lessons/class_JS/ClassJSLesson.tsx
+++ src/pages/lessons/class_JS/ClassJSLesson.tsx
@@ -225,6 +225,96 @@ const employee = new Employee("Alice", 30, "Developer")`,
             &nbsp; // "I work as a Developer"
           </p>
         </div>
+        <br />
+        <p>Геттеры и сеттеры (get и set):</p>
+        <div>
+          <div
+            onClick={() =>
+              copyToClipboard(
+                `class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+
+  get getName() {
+    return \`Name: \${this.name}, age: \${this.age}\`
+  }
+
+  set setAge(newAge) {
+    (Number.isInteger(newAge) && newAge > 0)
+    ? this.age = newAge
+    : console.error("Invalid age")
+  }
+}
+
+const person1 = new Person("Alice", 25)`,
+              )
+            }
+          >
+            class Person &#123;
+            <br />
+            &nbsp;&nbsp;constructor(name, age) &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;this.name = name
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;this.age = age
+            <br />
+            &#125;
+            <br />
+            <br />
+            &nbsp;&nbsp;get getName() &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;return `Name: $&#123;this.name&#125;, age:
+            $&#123;this.age&#125;`
+            <br />
+            &nbsp;&nbsp;&#125;
+            <br />
+            <br />
+            &nbsp;&nbsp;set setAge(newAge) &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;(Number.isInteger(newAge) && newAge &gt; 0)
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;? this.age = newAge
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;: console.error("Invalid age")
+            <br />
+            &nbsp;&nbsp;&#125;
+            <br />
+            &#125;
+            <br />
+            <br />
+            const person1 = new Person("Alice", 25)
+          </div>
+          <b>&nbsp;&nbsp;</b>
+          <p>
+            Геттеры и сеттеры вызываются как свойства (person1.getName и )
+            <br />
+            <br />
+            Геттеры не принимают аргументов.
+            <br />
+            <br />
+            Название геттера/сеттера не должно совпадать с this.&lt;название&gt;
+            внутри себя чтобы избежать рекурсии (они будут вызывать сами себя).
+            <br />
+            <br />
+            В данном примере сеттер проводит валидацию входящего числа (число
+            должно быть целое и больше 0).
+            <br />
+            <br />
+            <span
+              onClick={() => copyToClipboard('console.log(person1.greet())')}
+            >
+              console.log(person1.greet())
+            </span>
+            &nbsp; // "Hi, my name is Alice and I'm 25 years old"
+            <br />
+            <span onClick={() => copyToClipboard('console.log(Person.info())')}>
+              console.log(Person.info())
+            </span>
+            &nbsp; // "This is a Person class"
+          </p>
+        </div>
       </div>
     </>
   )

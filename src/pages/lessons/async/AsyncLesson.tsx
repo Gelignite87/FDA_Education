a0ebb9a5bbb3a20eaeb263d46902f848080720aa
dmitrii fomin <railgun87@rambler.ru>
2024-12-01 15:04:48 +1200

    67

--- src/pages/lessons/async/AsyncLesson.tsx
+++ src/pages/lessons/async/AsyncLesson.tsx
@@ -69,12 +69,115 @@ fetchData(data => console.log(data), 'Sample data')`,
           </div>
           <b>&nbsp;&nbsp;</b>
           <p>
-            Асинхронность через Callbacks, функция передаётся как аргумент
-            функции. Через 2 секунды сработает&nbsp;
+            Асинхронность через Callbacks (функция передаётся как аргумент
+            функции).
+            <br />
+            <br />
+            Через 2 секунды сработает&nbsp;
             <span onClick={() => copyToClipboard('console.log(data)')}>
               console.log(data)
             </span>
             &nbsp;и выведет строку 'Sample data'.
+          </p>
+        </div>
+        <br />
+        <div>
+          <div
+            onClick={() =>
+              copyToClipboard(
+                `function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Sample data")
+    }, 2000)
+  })
+}
+
+fetchData().then(data => console.log(data))`,
+              )
+            }
+          >
+            function fetchData() &#123;
+            <br />
+            &nbsp;&nbsp;return new Promise((resolve, reject) =&gt; &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;setTimeout(() =&gt; &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;resolve("Sample data")
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;, 2000)
+            <br />
+            &nbsp;&nbsp;&#125;)
+            <br />
+            &#125;
+            <br />
+            <br />
+            fetchData().then(data =&gt; console.log(data))
+          </div>
+          <b>&nbsp;&nbsp;</b>
+          <p>
+            Асинхронность через Promises.
+            <br />
+            <br />
+            Через 2 секунды сработает&nbsp;
+            <span onClick={() => copyToClipboard('resolve("Sample data")')}>
+              resolve("Sample data")
+            </span>
+            &nbsp;и через&nbsp;
+            <span
+              onClick={() =>
+                copyToClipboard('.then(data => console.log(data))')
+              }
+            >
+              .then(data =&gt; console.log(data))
+            </span>
+            &nbsp;попадет в console.log, который выведет строку 'Sample data'.
+          </p>
+        </div>
+        <br />
+        <div>
+          <div
+            onClick={() =>
+              copyToClipboard(
+                `async function fetchData() {
+  const data = await new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Sample data'), 2000)
+  })
+  console.log(data)
+}
+
+fetchData()`,
+              )
+            }
+          >
+            async function fetchData() &#123;
+            <br />
+            &nbsp;&nbsp;const data = await new Promise((resolve, reject) =&gt;
+            &#123;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;setTimeout(() =&gt; resolve('Sample data'),
+            2000)
+            <br />
+            &nbsp;&nbsp;&#125;)
+            <br />
+            &nbsp;&nbsp;console.log(data)
+            <br />
+            &#125;
+            <br />
+            <br />
+            fetchData()
+          </div>
+          <b>&nbsp;&nbsp;</b>
+          <p>
+            Асинхронность через Async/Await.
+            <br />
+            <br />
+            Через 2 секунды сработает&nbsp;
+            <span onClick={() => copyToClipboard('resolve("Sample data")')}>
+              resolve("Sample data")
+            </span>
+            , запишется в константу data и попадет в console.log, который
+            выведет строку 'Sample data'.
           </p>
         </div>
       </div>
